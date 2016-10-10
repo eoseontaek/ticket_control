6e1f343c23507da6241607125c5bc56f4d42cc14
eoseontaek <eosuntaek@gmail.com>
2016-10-10 21:07:58 +0900

    Add the jfxrt.jar for JavaFX.

--- TicketControl/src/ticketServer/TicketServer.java
+++ TicketControl/src/ticketServer/TicketServer.java
@@ -1,5 +1,19 @@
 package ticketServer;
 
-public class TicketServer {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicketServer extends Application{
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
