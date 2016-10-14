5602306ef04dabe8a9c1981cabf3d4bc329909ba
eoseontaek <eosuntaek@gmail.com>
2016-10-14 14:55:31 +0900

    관리자 메인 stage 등록

--- TicketControl/src/ticketServer/logIn/LogInContoller.java
+++ TicketControl/src/ticketServer/logIn/LogInContoller.java
@@ -1,12 +1,17 @@
 package ticketServer.logIn;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LogInContoller implements Initializable{
 	@FXML private Button btnLogIn;
@@ -19,7 +24,22 @@ public class LogInContoller implements Initializable{
 	}
 	
 	public void handleBtnLogInAction(ActionEvent event){
-		System.out.println("login");
+		
+		////// 로그인 처리 //////////////////////////////////////////
+		
+		
+		
+		///////////////////////////////////////////////////////////		
+		
+		try {
+			Parent administrator = FXMLLoader.load(getClass().getResource("..\\administratorMain\\AdministratorMain.fxml"));
+			Scene scene = new Scene(administrator);
+			Stage primaryStage = (Stage) btnLogIn.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 
