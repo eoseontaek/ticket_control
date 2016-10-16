4896db74564fb5e80e6c7bcdc50dea4510a32a36
eoseontaek <eosuntaek@gmail.com>
2016-10-16 18:20:46 +0900

    Revert "Revert "DAO 기본 구조 작성""
    This reverts commit e4380dfc6fcdb4b65be6b6d91e295c62cc06ee45.

---
+++ TicketControl/src/ticketServer/dao/CalculateBreakdownDAO.java
@@ -0,0 +1,43 @@
+package ticketServer.dao;
+
+import java.util.ArrayList;
+
+public class CalculateBreakdownDAO extends TicketDAO<CalculateBreakdown>{
+
+	@Override
+	public ArrayList<CalculateBreakdown> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CalculateBreakdown getData(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertData(CalculateBreakdown t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delData(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateData(CalculateBreakdown t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ TicketControl/src/ticketServer/dao/Menu.java
@@ -0,0 +1,60 @@
+package ticketServer.dao;
+
+public class Menu {
+	private int num;
+	private int menu_type;
+	private String rice;
+	private String soup;
+	private String sidedish1;
+	private String sidedish2;
+	private String sidedish3;
+	private String image;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getMenu_type() {
+		return menu_type;
+	}
+	public void setMenu_type(int menu_type) {
+		this.menu_type = menu_type;
+	}
+	public String getRice() {
+		return rice;
+	}
+	public void setRice(String rice) {
+		this.rice = rice;
+	}
+	public String getSoup() {
+		return soup;
+	}
+	public void setSoup(String soup) {
+		this.soup = soup;
+	}
+	public String getSidedish1() {
+		return sidedish1;
+	}
+	public void setSidedish1(String sidedish1) {
+		this.sidedish1 = sidedish1;
+	}
+	public String getSidedish2() {
+		return sidedish2;
+	}
+	public void setSidedish2(String sidedish2) {
+		this.sidedish2 = sidedish2;
+	}
+	public String getSidedish3() {
+		return sidedish3;
+	}
+	public void setSidedish3(String sidedish3) {
+		this.sidedish3 = sidedish3;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
