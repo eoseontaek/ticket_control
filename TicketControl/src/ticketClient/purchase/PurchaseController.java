136db4fc6d9d48297722734eaef3b1a143386e0d
jangsunghyuck <wkdtjdgur13@naver.com>
2016-10-16 16:58:47 +0900

    Client Action Event 추가

---
+++ TicketControl/src/ticketClient/purchase/PurchaseController.java
@@ -0,0 +1,51 @@
+package ticketClient.purchase;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class PurchaseController implements Initializable{
+
+	@FXML private Button prevBtn;
+	@FXML private Button menuChk;
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		prevBtn.setOnAction(event->handleprevBtnAction(event));
+		menuChk.setOnAction(event->handlemenuChkAction(event));
+
+	}
+	
+	public void handleprevBtnAction(ActionEvent event) {
+		try {
+			Parent prev = FXMLLoader.load(getClass().getResource("..\\Main\\Main.fxml"));
+			Scene scene = new Scene(prev);
+			Stage primaryStage = (Stage) prevBtn.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void handlemenuChkAction(ActionEvent event) {
+		try {
+			Parent menuchk = FXMLLoader.load(getClass().getResource("..\\menuresult\\menuResult.fxml"));
+			Scene scene = new Scene(menuchk);
+			Stage primaryStage = (Stage) menuChk.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+}
+
