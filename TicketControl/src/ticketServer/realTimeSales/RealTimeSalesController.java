7a34f2c2287b3207330d0d78d3943c024e94196d
eoseontaek <eosuntaek@gmail.com>
2016-10-16 14:52:01 +0900

    실시간 매출(Real Time Sales) ActionEvent 등록

--- TicketControl/src/ticketServer/realTimeSales/RealTimeSalesController.java
+++ TicketControl/src/ticketServer/realTimeSales/RealTimeSalesController.java
@@ -15,11 +15,18 @@ import javafx.stage.Stage;
 
 public class RealTimeSalesController implements Initializable{
 	@FXML private Button btnHome;
-
+	@FXML private Button btnSalesStatistics;
+	@FXML private Button btnBalanceAccounts;
+	@FXML private Button btnMealMenu;
+	@FXML private Button btnConfiguration;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		btnHome.setOnAction(event->handleBtnHomeAction(event));
-		
+		btnSalesStatistics.setOnAction(event->handlebtnSalesStatisticsAction(event));
+		btnBalanceAccounts.setOnAction(event->handleBtnBalanceAccountsAction(event));
+		btnMealMenu.setOnAction(event->handleBtnMealMenuAction(event));
+		btnConfiguration.setOnAction(event->handleBtnConfigurationAction(event));
 	}
 	
 	
@@ -37,4 +44,47 @@ public class RealTimeSalesController implements Initializable{
 		}
 	}
 	
+	public void handlebtnSalesStatisticsAction(ActionEvent event){
+		try {
+			Parent salesStatisticsRoot = FXMLLoader.load(getClass().getResource("..\\salesStatistics\\SalesStatistics.fxml"));
+			Scene scene = new Scene(salesStatisticsRoot);
+			Stage primaryStage = (Stage) btnSalesStatistics.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void handleBtnBalanceAccountsAction(ActionEvent event){
+		try {
+			Parent balanceAccountsRoot = FXMLLoader.load(getClass().getResource("..\\balanceAccounts\\BalanceAccounts.fxml"));
+			Scene scene = new Scene(balanceAccountsRoot);
+			Stage primaryStage = (Stage) btnBalanceAccounts.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void handleBtnMealMenuAction(ActionEvent event){
+		try {
+			Parent mealMenuRoot = FXMLLoader.load(getClass().getResource("..\\mealMenu\\MealMenu.fxml"));
+			Scene scene = new Scene(mealMenuRoot);
+			Stage primaryStage = (Stage) btnMealMenu.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void handleBtnConfigurationAction(ActionEvent event){
+		try {
+			Parent configurationRoot = FXMLLoader.load(getClass().getResource("..\\configuration\\Configuration.fxml"));
+			Scene scene = new Scene(configurationRoot);
+			Stage primaryStage = (Stage) btnConfiguration.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
