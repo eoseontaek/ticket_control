a034e07cf6be2cf882146f7ac354268a89156315
eoseontaek <eosuntaek@gmail.com>
2016-10-17 13:55:20 +0900

    로그인 컨트롤러 TextField, PasswordField 추가.

--- TicketControl/src/ticketServer/logIn/LogInContoller.java
+++ TicketControl/src/ticketServer/logIn/LogInContoller.java
@@ -11,10 +11,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LogInContoller implements Initializable{
 	@FXML private Button btnLogIn;
+	@FXML private TextField txtFieldID;
+	@FXML private PasswordField pwFieldPW;
+	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -26,21 +31,24 @@ public class LogInContoller implements Initializable{
 	public void handleBtnLogInAction(ActionEvent event){
 		
 		////// 로그인 처리 //////////////////////////////////////////
-		
+		System.out.println("id :" + txtFieldID.getText());
+		System.out.println("pw :" + pwFieldPW.getText());
 		
 		
 		///////////////////////////////////////////////////////////		
-		
-		try {
-			Parent administrator = FXMLLoader.load(getClass().getResource("..\\administratorMain\\AdministratorMain.fxml"));
-			Scene scene = new Scene(administrator);
-			Stage primaryStage = (Stage) btnLogIn.getScene().getWindow();
-			primaryStage.setScene(scene);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (true) { // id, pw 일치하면...
+			try {
+				Parent administrator = FXMLLoader
+						.load(getClass().getResource("..\\administratorMain\\AdministratorMain.fxml"));
+				Scene scene = new Scene(administrator);
+				Stage primaryStage = (Stage) btnLogIn.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else { // id, pw 일치하지 않으면...
+			
 		}
 	}
-	
-
 }
