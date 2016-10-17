9345a791b4b4ec77a77503203879a7ac76a0cdb2
eoseontaek <eosuntaek@gmail.com>
2016-10-17 16:28:26 +0900

    "real_time_sales"  테이블 DAO 기능 구현.

--- TicketControl/src/ticketServer/dao/RealTimeSalesDAO.java
+++ TicketControl/src/ticketServer/dao/RealTimeSalesDAO.java
@@ -7,21 +7,70 @@ public class RealTimeSalesDAO extends TicketDAO<RealTimeSales>{
 
 	@Override
 	public ArrayList<RealTimeSales> getAll() {
+		connectDB();
 		
-		return null;
+		sql = "select * from real_time_sales;";
+		
+		ArrayList<RealTimeSales> rtsList = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			rtsList = new ArrayList<>();
+			
+			while(rs.next()){
+				int num = rs.getInt("num");
+				String purchaser_data = rs.getString("purchaser_data");
+				String purchaser_time = rs.getString("purchaser_time");
+				int purchase_amount = rs.getInt("purchase_amount");
+				
+				RealTimeSales rts = new RealTimeSales(num, purchaser_data, purchaser_time, purchase_amount);
+				rtsList.add(rts);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeDB();
+		}
+		
+		return rtsList;
 	}
 
 	@Override
 	public RealTimeSales getData(int index) {
+		connectDB();
+
+		sql = "select * from real_time_sales where num=?;";
+		RealTimeSales rts = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, index);
+			rs = pstmt.executeQuery();
+			
+			rs.next();
+			rts = new RealTimeSales();
+			rts.setNum(rs.getInt("num"));
+			rts.setPurchaser_data(rs.getString("purchaser_data"));
+			rts.setPurchaser_time(rs.getString("purchaser_time"));
+			rts.setPurchase_amount(rs.getInt("purchase_amount"));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeDB();
+		}
 		
-		return null;
+		return rts;
 	}
 
 	@Override
 	public boolean insertData(RealTimeSales t) {
 		connectDB();
 		
-		sql = "insert into real_time_sales(purchaser_data,purchaser_time,purchase_amount) values(?,?,?)";
+		sql = "insert into real_time_sales(purchaser_data,purchaser_time,purchase_amount) values(?,?,?);";
 		try {
 			pstmt = connection.prepareStatement(sql);
 			pstmt.setString(1, t.getPurchaser_data());
@@ -40,17 +89,60 @@ public class RealTimeSalesDAO extends TicketDAO<RealTimeSales>{
 
 	@Override
 	public boolean delData(int index) {
-		return false;
+		connectDB();
+		
+		sql = "delete from real_time_sales where num=?;";
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, index);
+			
+			int result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeDB();
+		}
+		return true;
 	}
 
 	@Override
 	public boolean delAll() {
-		return false;
+		connectDB();
+
+		sql = "delete from real_time_sales;";
+		try {
+			pstmt = connection.prepareStatement(sql);
+			int result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeDB();
+		}
+		return true;
 	}
 
 	@Override
 	public boolean updateData(RealTimeSales t) {
-		return false;
+		connectDB();
+		
+		sql = "update real_time_sales set num=?,purchaser_data=?,purchaser_time=?,purchase_amount=?;";
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, t.getNum());
+			pstmt.setString(2, t.getPurchaser_data());
+			pstmt.setString(3, t.getPurchaser_time());
+			pstmt.setInt(4, t.getPurchase_amount());
+			int result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeDB();
+		}
+		return true;
 	}
 
 }
