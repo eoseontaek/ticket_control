ee18a9d18e420c506b80495456baf15256fe9f3c
yunmi0701 <apple_yun@naver.com>
2016-10-20 20:06:27 +0900

    barcode scan gun

---
+++ TicketControl/src/ticketServer/BarcodeScan.java
@@ -0,0 +1,50 @@
+package ticketServer;
+
+import java.awt.Button;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import ticketServer.dao.Barcode;
+import ticketServer.dao.BarcodeDAO;
+
+public class BarcodeScan extends Thread{
+	public void run(){
+		while(true){
+			InputStreamReader br = new InputStreamReader(System.in);
+			BufferedReader field = new BufferedReader(br);
+			JFrame frame = new JFrame("식권 확인");
+			
+			String scanned;
+			try {
+				scanned = field.readLine();
+				if(scanned.length()==13){
+					String scannedBarcode = scanned.substring(0, 12);
+					BarcodeDAO dao = new BarcodeDAO();
+					
+					if(dao.getUseOX(scannedBarcode)==0){
+						JOptionPane.showMessageDialog(frame, "감사합니다.");
+						System.exit(0);
+					}
+					if(dao.getUseOX(scannedBarcode)==1){
+						JOptionPane.showMessageDialog(frame, "이미 사용 된 식권입니다.");
+						System.exit(0);
+					}
+				}
+				
+			} catch (IOException e) {
+				System.out.println("오류");
+			}
+		
+		}		
+	}
+}
--- TicketControl/src/ticketServer/TicketServer.java
+++ TicketControl/src/ticketServer/TicketServer.java
@@ -1,6 +1,8 @@
 package ticketServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousChannelGroup;
@@ -34,6 +36,7 @@ public class TicketServer extends Application{
 //	private static final String SERVER_IP = "70.12.109.100";
 	private static final int SERVER_PORT = 6001;
 	private static final int BUFFER_SIZE = 1024;
+	
 	
 	
 	class Client{
@@ -212,6 +215,15 @@ public class TicketServer extends Application{
 		stage.setOnCloseRequest(event->stopServer());
 		stage.setScene(new Scene(logInRoot));
 		stage.show();
+		
+		//////////////////////////////////////////////////////////////////////
+		
+		Thread thread = new BarcodeScan();
+		thread.setDaemon(true);
+		thread.start();
+		
+		///////////////////////////////////////////////////////////////////////
+		
 	}
 	
 	public static void main(String[] args) {
